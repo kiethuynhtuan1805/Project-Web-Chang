d3ff7ebd9b6d214a2dea31a3fa2f6d384318851a
kiethuynhtuan1805 <142136277+kiethuynhtuan1805@users.noreply.github.com>
2024-10-17 15:19:45 +0700

    Delete src/Redux directory

--- src/Redux/Action/actUser.js
+++
@@ -1,6 +0,0 @@
-const actUser = {
-    GET_USER: "GET_USER",
-    SET_USER: "SET_USER"
-}
-
-export default actUser;
--- src/Redux/rootSaga.js
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import globalSaga from "Pages/Global/globalSaga";
-
-export default function* rootSaga() {
-  yield all([
-    globalSaga(),
-  ]);
-}
